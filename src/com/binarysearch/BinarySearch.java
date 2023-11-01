16e41e8f4ef53634ff015638993da4a56b2633e5
igaurav177 <gauravkumarsharma2001@gmail.com>
2023-11-01 21:44:54 +0530

    Changes have been committed.

---
+++ src/com/binarysearch/BinarySearch.java
@@ -0,0 +1,27 @@
+package com.binarysearch;
+
+public class BinarySearch {
+	public static int binarySearch(int arr[], int target) {
+		int start=0;
+		int end=arr.length-1;
+		while(start<end) {
+			int mid=(start+(end-start))/2;
+			if(arr[mid]>target) {
+				end=mid-1;
+			}else if(arr[mid]<target) {
+				start=mid+1;
+			}
+			else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {10,20,40,60,80};
+		System.out.println(binarySearch(arr, 40));
+ 
+	}
+
+}
