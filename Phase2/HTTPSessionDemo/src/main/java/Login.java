888e969e0246cd104ee7babe4e85dc92ba8affe1
igaurav177 <gauravkumarsharma2001@gmail.com>
2023-11-07 19:29:23 +0530

    all files are added

---
+++ Phase2/HTTPSessionDemo/src/main/java/Login.java
@@ -0,0 +1,61 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw=response.getWriter();
+		RequestDispatcher rd=request.getRequestDispatcher("index.html");
+		rd.include(request, response);
+		String user=request.getParameter("user");
+		String password=request.getParameter("pwd");
+		if(user.equals(password)) {
+			
+			
+			// httpSession 
+			HttpSession session=request.getSession();
+			session.setAttribute("userid",user);
+
+			pw.print("login is successfull");
+			
+		}
+		else {
+			pw.print("login failed ");
+		}
+
+	}
+
+}
